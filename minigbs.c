af6ee7aa83b594107e8318af38b7b7ead9a17393
Mahyar Koshkouei <mk@deltabeard.com>
2020-01-07 10:42:23 +0000

    minigbs: change defines to use elif
    Signed-off-by: Mahyar Koshkouei <mk@deltabeard.com>

--- minigbs.c
+++ minigbs.c
@@ -817,7 +817,7 @@ int main(int argc, char **argv)
 
 	audio_init();
 
-#ifdef AUDIO_DRIVER_SDL
+#if defined(AUDIO_DRIVER_SDL)
 	/* Initialise SDL audio. */
 	{
 		SDL_AudioDeviceID audio;
@@ -847,8 +847,7 @@ int main(int argc, char **argv)
 		/* Begin playing audio. */
 		SDL_PauseAudioDevice(audio, 0);
 	}
-#endif
-#ifdef AUDIO_DRIVER_SOKOL
+#elif defined(AUDIO_DRIVER_SOKOL)
 	/* Initialise SOKOL Audio. */
 	{
 		const saudio_desc sd = {
@@ -859,8 +858,7 @@ int main(int argc, char **argv)
 		};
 		saudio_setup(&sd);
 	}
-#endif
-#ifdef AUDIO_DRIVER_MINIAL
+#elif defined(AUDIO_DRIVER_MINIAL)
 	mal_device device;
 	mal_context audio_ctx;
 	mal_device_config config;
@@ -887,6 +885,10 @@ int main(int argc, char **argv)
 			return -4;
 		}
 	}
+#elif defined(AUDIO_DRIVER_NONE)
+	float *samples = malloc(AUDIO_SAMPLE_RATE * sizeof(float));
+#else
+#error "No audio driver defined."
 #endif
 
 	/* Fixes printf's not printing to stdout until exit in Windows. */
@@ -919,17 +921,20 @@ int main(int argc, char **argv)
 			}
 			break;
 		}
+#if defined(AUDIO_DRIVER_NONE)
+		audio_callback(NULL, (uint8_t *)samples, AUDIO_SAMPLE_RATE * sizeof(float));
+#endif
 	}
 
 out:
-#ifdef AUDIO_DRIVER_SDL
+#if defined(AUDIO_DRIVER_SDL)
 	SDL_Quit();
-#endif
-#ifdef AUDIO_DRIVER_SOKOL
+#elif defined(AUDIO_DRIVER_SOKOL)
 	saudio_shutdown();
-#endif
-#ifdef AUDIO_DRIVER_MINIAL
+#elif defined(AUDIO_DRIVER_MINIAL)
 	mal_device_uninit(&device);
+#elif defined(AUDIO_DRIVER_NONE)
+	free(samples);
 #endif
 
 	audio_deinit();
